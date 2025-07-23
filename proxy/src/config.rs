3401077594fad3954c4b0821419012d3d6af8e80
kneeyo1 <neo.huang@sentry.io>
2025-07-23 14:19:11 -0700

    Parsing an example config rules file

--- proxy/src/config.rs
+++ proxy/src/config.rs
@@ -1,13 +1,10 @@
 use serde::Deserialize;
-use std::fs::File;
-use std::error::Error;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub routes: Vec<Route>,
 }
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Route {
     #[serde(rename = "match")]
     pub match_criteria: Match,
@@ -15,22 +12,13 @@ pub struct Route {
     pub route_action: RouteAction,
 }
 
-#[derive(Debug, Deserialize)]
 pub struct Match {
     pub host: String,
     pub path_prefix_pattern: String,
 }
 
-#[derive(Debug, Deserialize)]
 pub struct RouteAction {
     pub dynamic_to: Option<String>,
     pub to: Option<String>,
     pub default: Option<String>,
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("example_config.yaml")?;
-    let parsed_config: Config = serde_yaml::from_reader(file)?;
-    println!("\n--- Parsed Config ---\n{:#?}", parsed_config);
-    Ok(())
 }

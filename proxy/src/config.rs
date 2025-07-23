c3ccb0ff47feac5605f4599bd53d88c46d0ef5d6
kneeyo1 <neo.huang@sentry.io>
2025-07-23 14:25:30 -0700

    updated to move to own file

--- proxy/src/config.rs
+++ proxy/src/config.rs
@@ -1,10 +1,13 @@
 use serde::Deserialize;
+use std::fs::File;
+use std::error::Error;
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize)]
 pub struct Config {
     pub routes: Vec<Route>,
 }
-#[derive(Debug, Deserialize, Clone)]
+
+#[derive(Debug, Deserialize)]
 pub struct Route {
     #[serde(rename = "match")]
     pub match_criteria: Match,
@@ -12,13 +15,22 @@ pub struct Route {
     pub route_action: RouteAction,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct Match {
     pub host: String,
     pub path_prefix_pattern: String,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct RouteAction {
     pub dynamic_to: Option<String>,
     pub to: Option<String>,
     pub default: Option<String>,
+}
+
+
+pub fn load_from_file(path: &str) -> Result<Config, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let parsed_config: Config = serde_yaml::from_reader(file)?;
+    Ok(parsed_config)
 }
--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,36 +1,9 @@
-use serde::Deserialize;
-use std::fs::File;
 use std::error::Error;
 
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    pub routes: Vec<Route>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Route {
-    #[serde(rename = "match")]
-    pub match_criteria: Match,
-    #[serde(rename = "route")]
-    pub route_action: RouteAction,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Match {
-    pub host: String,
-    pub path_prefix_pattern: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct RouteAction {
-    pub dynamic_to: Option<String>,
-    pub to: Option<String>,
-    pub default: Option<String>,
-}
+mod config;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("example_config.yaml")?;
-    let parsed_config: Config = serde_yaml::from_reader(file)?;
+    let parsed_config = config::load_from_file("example_config.yaml")?;
     println!("\n--- Parsed Config ---\n{:#?}", parsed_config);
     Ok(())
 }

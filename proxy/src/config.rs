5bfe765af09f1b7788416d455d02d2644a2ccfb6
Nikhar Saxena <84807402+nikhars@users.noreply.github.com>
2025-07-28 13:40:45 -0700

    ci(lint): Allow dead code (#6)

--- proxy/src/config.rs
+++ proxy/src/config.rs
@@ -2,23 +2,27 @@ use serde::Deserialize;
 use std::error::Error;
 use std::fs::File;
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub routes: Vec<Route>,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Route {
     pub r#match: Match,
     pub route: RouteAction,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Match {
     pub host: String,
     pub path_prefix_pattern: Option<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
 pub enum RouteAction {

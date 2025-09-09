e9ed5259fcd37104d4d9c6b51091fa7f3b2bd3dd
kneeyo1 <neo.huang@sentry.io>
2025-09-09 15:13:47 -0700

    pulled out the rules engine, using hyper

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,9 +1,20 @@
 use std::error::Error;
+use std::net::SocketAddr;
 
 mod config;
+mod proxy;
+mod rules_engine;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let parsed_config = config::load_from_file("example_config.yaml")?;
-    println!("\n--- Parsed Config ---\n{parsed_config:#?}");
+    println!("Loaded configuration with {} routes", parsed_config.routes.len());
+    
+    let proxy_server = proxy::ProxyServer::new(parsed_config);
+    let addr: SocketAddr = "127.0.0.1:8080".parse()?;
+    
+    println!("Starting proxy server on {}", addr);
+    proxy_server.run(addr).await?;
+    
     Ok(())
 }
--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -1,40 +1,86 @@
-use crate::config::{Config, Route, RouteAction};
+use std::sync::Arc;
+use hyper::{Body, Client, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-pub struct IncomingRequest<'a> {
-    pub host: &'a str,
-    pub path: &'a str,
+use crate::config::Config;
+use crate::rules_engine::{RulesEngine, IncomingRequest};
+
+pub struct ProxyServer {
+    rules_engine: Arc<RulesEngine>,
+    client: Client<hyper::client::HttpConnector>,
 }
 
-pub struct RulesEngine {
-    pub routes: Vec<Route>,
-}
+impl ProxyServer {
+    pub fn new(config: Config) -> Self {
+        let rules_engine = Arc::new(RulesEngine::new(config.routes));
+        let client = Client::new();
 
-impl RulesEngine {
-    
-    // finds the route destination that matches the incoming request
-    pub fn find_destination(&self, request: &IncomingRequest) -> Option<String> {
-        for route in &self.routes {
-            if route.r#match.host != request.host {
-                continue;
-            }
-
-            let path_matches = match &route.r#match.path_prefix_pattern {
-                Some(pattern) => {
-                    let static_prefix = pattern.split('{').next().unwrap_or("");
-                    request.path.starts_with(static_prefix)
-                },
-                // If no pattern is defined, any path is considered a match.
-                None => true
-            };
-            
-            if !path_matches {
-                continue;
-            }
-
-            return match &route.route {
-                RouteAction::Static { to } => Some(to.clone()),
-                RouteAction::Dynamic { default, .. } => default.clone(),
-            };
+        Self {
+            rules_engine,
+            client,
         }
+    }
+
+    pub async fn run(&self, addr: SocketAddr) -> Result<(), hyper::Error> {
+        let proxy_server = Arc::new(self);
+        
+        let make_svc = make_service_fn(move |_conn| {
+            let proxy_server = Arc::clone(&proxy_server);
+            async move {
+                Ok::<_, Infallible>(service_fn(move |req| {
+                    let proxy_server = Arc::clone(&proxy_server);
+                    async move { proxy_server.handle_request(req).await }
+                }))
+            }
+        });
+
+        let server = Server::bind(&addr).serve(make_svc);
+        server.await
+    }
+
+    async fn handle_request(&self, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+        let host = req.headers()
+            .get("host")
+            .and_then(|h| h.to_str().ok())
+            .unwrap_or("");
+        
+        let path = req.uri().path();
+        
+        let incoming_request = IncomingRequest { host, path };
+        
+        match self.rules_engine.find_destination(&incoming_request) {
+            Some(destination) => {
+                match self.forward_request(req, &destination).await {
+                    Ok(response) => Ok(response),
+                    Err(_) => Ok(Response::builder()
+                        .status(502)
+                        .body(Body::from("Bad Gateway"))
+                        .unwrap()),
+                }
+            }
+            None => Ok(Response::builder()
+                .status(404)
+                .body(Body::from("Not Found"))
+                .unwrap()),
+        }
+    }
+
+    async fn forward_request(&self, mut req: Request<Body>, destination: &str) -> Result<Response<Body>, hyper::Error> {
+        // Modify request URI to point to destination
+        let uri = format!("{}{}", destination, req.uri().path_and_query().map(|pq| pq.as_str()).unwrap_or(""));
+        *req.uri_mut() = uri.parse().unwrap();
+        
+        // Remove hop-by-hop headers
+        req.headers_mut().remove("connection");
+        req.headers_mut().remove("proxy-connection");
+        req.headers_mut().remove("te");
+        req.headers_mut().remove("trailer");
+        req.headers_mut().remove("transfer-encoding");
+        req.headers_mut().remove("upgrade");
+        
+        // Forward the request
+        self.client.request(req).await
     }
 }

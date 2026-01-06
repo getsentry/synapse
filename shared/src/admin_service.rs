ab35572997e25d01dc3d922c6ca298d67c4c7da0
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-01-06 14:53:21 -0800

    wip

---
+++ shared/src/admin_service.rs
@@ -0,0 +1,45 @@
+use crate::errors::ProxyError;
+use http_body_util::combinators::BoxBody;
+use http_body_util::{BodyExt, Full};
+use hyper::body::{Bytes, Incoming};
+use hyper::service::Service;
+use hyper::{Request, Response, StatusCode};
+use locator::client::Locator;
+use shared::http::make_boxed_error_response;
+use std::future::Future;
+use std::pin::Pin;
+
+pub struct AdminService {
+    locator: Locator,
+}
+
+impl AdminService {
+    pub fn new(locator: Locator) -> Self {
+        Self { locator }
+    }
+}
+
+impl Service<Request<Incoming>> for AdminService {
+    type Response = Response<BoxBody<Bytes, ProxyError>>;
+    type Error = ProxyError;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+
+    fn call(&self, req: Request<Incoming>) -> Self::Future {
+        let is_ready = self.locator.is_ready();
+
+        Box::pin(async move {
+            let res = match req.uri().path() {
+                "/health" => {
+                    Response::new(Full::new("ok\n".into()).map_err(|e| match e {}).boxed())
+                }
+                "/ready" => match is_ready {
+                    true => Response::new(Full::new("ok\n".into()).map_err(|e| match e {}).boxed()),
+                    false => make_boxed_error_response(StatusCode::SERVICE_UNAVAILABLE),
+                },
+                _ => make_boxed_error_response(StatusCode::NOT_FOUND),
+            };
+            Ok(res)
+        })
+    }
+}

44d087d3ef1b6fe5f598fd6497b39f04a917d9ec
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-01-07 10:55:21 -0800

    .

--- ingest-router/src/lib.rs
+++ ingest-router/src/lib.rs
@@ -13,21 +13,42 @@ mod testutils;
 
 use crate::errors::IngestRouterError;
 use locator::client::Locator;
+use shared::admin_service::AdminService;
 use shared::http::run_http_service;
 
 pub async fn run(config: config::Config) -> Result<(), IngestRouterError> {
     let locator = Locator::new(config.locator.to_client_config()).await?;
 
     let ingest_router_service = ingest_router_service::IngestRouterService::new(
-        router::Router::new(config.routes, config.locales, locator),
+        router::Router::new(config.routes, config.locales, locator.clone()),
         config.relay_timeouts,
     );
+    let admin_service = AdminService::new({
+        let locator = locator.clone();
+        move || locator.is_ready()
+    });
 
     let router_task = run_http_service(
         &config.listener.host,
         config.listener.port,
         ingest_router_service,
     );
-    router_task.await?;
+    let admin_task = run_http_service(
+        &config.admin_listener.host,
+        config.admin_listener.port,
+        admin_service,
+    );
+
+    tokio::select! {
+        result = async { tokio::try_join!(router_task, admin_task) } => {
+            result?;
+        }
+        _ = tokio::signal::ctrl_c() => {
+            tracing::info!("Shutting down ingest-router...");
+        }
+    }
+
+    locator.shutdown().await;
+
     Ok(())
 }
